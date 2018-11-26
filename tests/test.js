0b7723193d7863931856ea99af8987135c68ee46
Jason Byrne <jason.byrne@flocasts.com>
2018-11-26 00:56:37 -0500

    making it testable with local mocks

--- tests/test.js
+++ tests/test.js
@@ -1,20 +1,35 @@
-let Flagpole = require('../dist/index.js').Flagpole;
+const Flagpole = require('../dist/index.js').Flagpole;
+const nock = require('nock');
+const fs = require('fs');
+
+let mockRoutes = nock('http://test.flagpole');
+
+mockRoutes.get('/test.html')
+    .reply(200, function (uri, requestBody) {
+        return fs.createReadStream(__dirname + '/mock/test.html');
+    });
+
+mockRoutes.get('/test2.html')
+    .reply(200, function (uri, requestBody) {
+        return fs.createReadStream(__dirname + '/mock/test2.html');
+    });
 
 let suite = Flagpole.Suite('Test')
+    .base('http://test.flagpole')
     .setConsoleOutput(false)
     .onDone(function (suite) {
         suite.print();
-    });;
+    });
 
 suite.Scenario('Test 1')
-    .mock('../tests/mock/test.html')
+    .open('/test.html')
     .assertions(function (test) {
         test.status().equals(200);
 
         test.select('a').length().greaterThan(0);
 
-        test.select('a[href^="http"]').each(function (link) {
-            link.tagName().equals('a');
+        test.select('a[href^="/"]').each(function (link) {
+            link.click('Test 2', true);
         });
 
     });
