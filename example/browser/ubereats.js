82cb6f35651a264b55ecd5f4fb69e9d4697317c4
Jason Byrne <jason.byrne@flocasts.com>
2019-07-29 01:11:54 -0400

    remove old code from uber eats example

--- example/browser/ubereats.js
+++ example/browser/ubereats.js
@@ -41,53 +41,5 @@ suite.Browser('Start on homepage and find local restaurants', { headless: false 
         this.assert('Restaurant has a name', (await this.text(paths.restaurantHeader)).trim().length)
             .greaterThan(0);
         return this.pause(1);
-    })
-    /*
-    .then(function () {
-        return this.page.click(paths.addressInput);
-    })
-    .then('Typing in the address', function () {
-        return this.page.type(paths.addressInput, address);
-    })
-    .then(async function () {
-        const value = await this.page.$eval(paths.addressInput, el => el.value);
-        return this.asyncAssert(value == address, 'Address field is ' + address);
-    })
-    .then(function () {
-        return this.resolves(
-            this.page.waitForSelector(paths.selectAddressButton, { timeout: 2000 }),
-            'Address selection dropdown shows up'
-        );
-    })
-    .then('Select the address and click submit', function () {
-        return this.page.click(paths.selectAddressButton);
-    })
-    .then(function () {
-        return this.page.click(paths.submitButton);
-    })
-    .then('Wait for results', function () {
-        return this.resolves(
-            this.page.waitForSelector(paths.orlandoRestaurantResults, { timeout: 10000 }),
-            'Restaurant results show up'
-        );
-    })
-    .then(async function () {
-        const results = await this.page.$$(paths.orlandoRestaurantResults);
-        return this.asyncAssert(() => {
-            return results.length > 5
-        }, 'There are more than five restaurants to choose from');
-    })
-    .then('Click on first result', function () {
-        return this.page.click(paths.orlandoRestaurantResults);
-    })
-    .then('Waiting for restaurant page to load', function () {
-        return this.resolves(
-            this.page.waitForSelector(paths.restaurantHeader, { timeout: 1000 }),
-            'Found restaurant page header'
-        );
-    })
-    .then(async function () {
-        const text = await this.page.$eval(paths.restaurantHeader, el => el.textContent);
-        return this.asyncAssert(text.trim().length > 0, 'Has restaurant name: ' + text);
     });
-    */
+    
