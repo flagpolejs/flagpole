1e9791f8252f7a3a83a40f3874f89a411344250a
Jason Byrne <jason.byrne@flocasts.com>
2019-08-15 17:37:17 -0400

    v2.0.26 with Alex recommendation to export all the useful classes for TS

--- src/flagpole.ts
+++ src/flagpole.ts
@@ -1,6 +1,34 @@
+
+import { Assertion } from "./assertion";
+import { AssertionContext } from "./assertioncontext";
+import { AssertionResult } from "./assertionresult";
+import { Browser, BrowserOptions } from "./browser";
+import { BrowserResponse } from "./browserresponse";
+import { CssResponse } from "./cssresponse";
+import { DOMElement } from "./domelement";
+import { ExtJSResponse } from "./extjsresponse";
+import { HtmlResponse } from "./htmlresponse";
+import { ImageResponse } from "./imageresponse";
+import { jPath, iJPath } from "./jpath";
+import { JsonResponse } from "./jsonresponse";
+import { ResourceResponse } from "./resourceresponse";
+import { ResponseType, GenericResponse, NormalizedResponse } from "./response";
+import { Scenario } from "./scenario";
+import { ScriptResponse } from "./scriptresponse";
 import { Suite } from "./suite";
+import { Value, iValue } from "./value";
+import { VideoResponse } from "./videoresponse";
 
 const cheerio = require('cheerio');
+
+export {
+    Suite, Scenario,
+    Browser, BrowserOptions, BrowserResponse, CssResponse, DOMElement, ExtJSResponse,
+    HtmlResponse, ImageResponse, JsonResponse, ResourceResponse,
+    ResponseType, GenericResponse, NormalizedResponse,
+    ScriptResponse, iValue, Value, VideoResponse,
+    jPath, iJPath
+};
 
 export enum FlagpoleOutput {
     console = 1,
