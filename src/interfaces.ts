7a00838353c3f032302b4d827905f3548c2607a6
jasonbyrne <jason.byrne@flocasts.com>
2020-09-07 14:36:56 -0400

    v2.5.5

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -444,14 +444,14 @@ export interface iAssertionContext {
     matches: RegExp,
     opts?: FindOptions
   ): Promise<iValue>;
-  existsAll(selector: string, opts?: FindOptions): Promise<iValue[]>;
+  existsAll(selector: string | string[], opts?: FindOptions): Promise<iValue[]>;
   existsAll(
-    selector: string[],
+    selector: string | string[],
     contains: string,
     opts?: FindOptions
   ): Promise<iValue[]>;
   existsAll(
-    selector: string[],
+    selector: string | string[],
     matches: RegExp,
     opts?: FindOptions
   ): Promise<iValue[]>;
