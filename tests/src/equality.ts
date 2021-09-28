14f90b39dec4723f43cb4b195bb151c9014d7041
John Sickels <johnwsickels@gmail.com>
2021-09-28 12:33:55 -0400

    cut the fat from the test

--- tests/src/equality.ts
+++ tests/src/equality.ts
@@ -3,8 +3,8 @@ import flagpole from "../../dist/index";
 const suite = flagpole("Equals");
 
 suite
-  .json("With undefined and assertion titles")
-  .open("https://reqres.in/api/users?page=1")
+  .resource("Test string, object, and array equality")
+  .mock()
   .next((context) => {
     context.assert("OK").equals("OK");
     context.assert({ foo: "bar" }).equals({ foo: "bar" });
