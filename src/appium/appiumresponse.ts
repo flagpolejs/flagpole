7d251f89064fa556c68cf4078d8ddf188096f90f
Nicholas Calaway <ncalaway@echelonfit.com>
2021-10-26 16:23:00 -0400

    Added ability to find an element

--- src/appium/appiumresponse.ts
+++ src/appium/appiumresponse.ts
@@ -1,15 +1,13 @@
 import { ProtoResponse } from "../response";
 import { HttpResponse } from "../httpresponse";
-import { iResponse, iValue } from "../interfaces";
+import { iResponse, iValue, FindOptions, FindAllOptions } from "../interfaces";
 import { ValuePromise } from "../value-promise";
 import { ScenarioType } from "../scenario-types";
 import { jpathFind, jpathFindAll, JPathProvider, JsonDoc } from "../json/jpath";
 import { HttpRequest } from "../httprequest";
+import { FlagpoleExecution } from "../flagpoleexecution";
 
-export class AppiumResponse
-  extends ProtoResponse
-  implements iResponse, JPathProvider
-{
+export class AppiumResponse extends ProtoResponse implements iResponse {
   public jsonDoc: JsonDoc | undefined;
 
   public get responseTypeName(): string {
@@ -20,14 +18,6 @@ export class AppiumResponse
     return "appium";
   }
 
-  public init(httpResponse: HttpResponse) {
-    super.init(httpResponse);
-    this.jsonDoc = new JsonDoc(this.jsonBody.$);
-    this.context
-      .assert("JSON is valid", this.jsonBody.$)
-      .type.not.equals("null");
-  }
-
   public getRoot(): any {
     return this.jsonBody.$;
   }
@@ -36,7 +26,39 @@ export class AppiumResponse
     throw "This type of scenario does not support eval.";
   }
 
-  public find = (path: string): ValuePromise => jpathFind(this, path);
+  public find(selector: string): ValuePromise {
+    return ValuePromise.execute(async () => {
+      const usingValue = selector.split("/");
+      const res = await this._findElement(
+        this.scenario.get("sessionId"),
+        usingValue[0],
+        usingValue[1]
+      );
+      res.json = JSON.parse(res.body);
+      this.jsonDoc = new JsonDoc(res.json);
+      return jpathFind(this, "value.ELEMENT");
+    });
+  }
+
   public findAll = (path: string): Promise<iValue[]> =>
     jpathFindAll(this, path);
+
+  private async _findElement(
+    sessionId: string,
+    using: string,
+    value: string
+  ): Promise<HttpResponse> {
+    const domain = FlagpoleExecution.global.environment?.defaultDomain || "";
+    const req = new HttpRequest({
+      method: "post",
+      uri: `${domain}session/${sessionId}/element`,
+      data: {
+        using: using,
+        value: value,
+      },
+    });
+    const res = await req.fetch();
+
+    return res;
+  }
 }
