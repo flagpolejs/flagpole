fa720cff0e6cd1aa591bb8d055c8488ba6366e9b
Nicholas Calaway <ncalaway@echelonfit.com>
2021-11-09 10:33:01 -0500

    Added JSDoc comments

--- src/appium/appium-helpers.ts
+++ src/appium/appium-helpers.ts
@@ -12,11 +12,22 @@ import { JsonDoc } from "../json/jpath";
 
 const DEFAULT_APPIUM_PORT = 4723;
 
+// Get URL prefix for sending Appium HTTP calls
+/*
+ * @param {iScenario} scenario - FlagPole scenario
+ * */
 const getUrlPrefix = (scenario: iScenario): string =>
   scenario.suite.baseUrl
     ? `${scenario.suite.baseUrl.protocol}//${scenario.suite.baseUrl.host}/wd/hub`
     : `http://localhost:${DEFAULT_APPIUM_PORT}/wd/hub`;
 
+// Send HTTP request to Appium server and return the response as a JsonDoc
+/*
+ * @param {iScenario} scenario - FlagPole scenario
+ * @param {string} path - Route to send request
+ * @param {HttpRequestOptions} opts - Additional options for request such as method, data, etc.
+ * @return {Promise<JsonDoc>} JSON object that is the result of the call to the Appium server
+ * */
 export const sendAppiumRequest = async (
   scenario: iScenario,
   path: string,
@@ -35,6 +46,12 @@ export const sendAppiumRequest = async (
   return doc;
 };
 
+// Get first returned Appium sessionId
+// This is necessary for all other HTTP calls to the Appium server
+/*
+ * @param {iScenario} scenario - FlagPole scenario
+ * @return {Promise<string>} sessionId to route all other HTTP requests
+ * */
 const getAppiumSession = async (scenario: iScenario) => {
   const json = await sendAppiumRequest(scenario, "/sessions", {
     method: "get",
@@ -42,6 +59,13 @@ const getAppiumSession = async (scenario: iScenario) => {
   return json.jsonRoot.value[0]?.id || null;
 };
 
+// Create a new Appium session
+// Returns sessionId from newly created session
+/*
+ * @param {Scenario} scenario - FlagPole scenario
+ * @param {any} opts - Appium session settings, called "capabilities"
+ * @return {Promise<string>} sessionId to route all other HTTP requests
+ * */
 const createAppiumSession = async (scenario: Scenario, opts: any = {}) => {
   const json = await sendAppiumRequest(scenario, "/session", {
     method: "post",
@@ -54,6 +78,13 @@ const createAppiumSession = async (scenario: Scenario, opts: any = {}) => {
   return json.jsonRoot.value.sessionId;
 };
 
+// Wrapper method which checks if an Appium session already exists
+// Returns scenario with existing or new Appium sessionId set to a scenario alias
+/*
+ * @param {Scenario} scenario - FlagPole scenario
+ * @param {any} opts - Appium session settings, called "capabilities"
+ * @return {Promise<Scenario>} Initial scenario with alias set
+ * */
 export const appiumSessionCreate = (scenario: Scenario, opts: any = {}) => {
   return async () => {
     const existingSessionId = await getAppiumSession(scenario);
@@ -70,6 +101,11 @@ export const appiumSessionCreate = (scenario: Scenario, opts: any = {}) => {
   };
 };
 
+// End Appium session
+/*
+ * @param {Scenario} scenario - FlagPole scenario
+ * @return {Promise<JsonDoc>} JSON response from DELETE call
+ * */
 export const appiumSessionDestroy = (scenario: Scenario) => {
   return async () => {
     const sessionId = scenario.get("sessionId");
@@ -79,6 +115,14 @@ export const appiumSessionDestroy = (scenario: Scenario) => {
   };
 };
 
+// Find by Android specific uiautomator selector
+/*
+ * @param {AppiumResponse} response - AppiumResponse which initiated findAll call referencing this method
+ * @param {string} selector - Selector passed in from findAll call referencing this method
+ * @param {string} text - Text value of element to be found, passed in from findAll call referencing this method
+ * @param {FindAllOptions | null} [opts] - Optional parameter which contains information on how many elements to return
+ * @return {Promise<iValue[]>} Elements found and possibly pared down by opts
+ * */
 export const appiumFindByUiAutomator = async (
   response: AppiumResponse,
   selector: string,
@@ -129,6 +173,11 @@ export const appiumFindByUiAutomator = async (
     : elements;
 };
 
+// Get package of Application Under Test
+/*
+ * @param {AppiumResponse} response - AppiumResponse which called this function
+ * @return {Promise<string>} Package name of Application Under Test
+ * */
 const appiumGetPackageName = async (
   response: AppiumResponse
 ): Promise<string> => {
@@ -144,6 +193,12 @@ const appiumGetPackageName = async (
   return res.jsonRoot.value;
 };
 
+// Get Appium session settings, called "capabilities"
+/*
+ * @param {string} sessionId - ID for currently running session
+ * @param {Scenario} scenario - FlagPole scenario
+ * @return {Promise<any>} - Object containing session capabilities
+ * */
 const getAppiumSessionCapabilities = async (
   sessionId: string,
   scenario: Scenario
@@ -155,6 +210,7 @@ const getAppiumSessionCapabilities = async (
   return res.jsonRoot.value;
 };
 
+// Appium HTTP call response which contains elementId
 type AppiumValueResponse = {
   [0]: string;
   ELEMENT: string;
