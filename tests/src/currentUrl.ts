7d4cf05b1a98529363d5f33fa2dd8d134077c1f7
John Sickels <johnwsickels@gmail.com>
2021-11-22 11:33:58 -0500

    fix currentUrl and visual, rename repro suite

--- tests/src/currentUrl.ts
+++ tests/src/currentUrl.ts
@@ -5,7 +5,12 @@ flagpole("Basic Smoke Test of Site", async (suite) => {
     .scenario("Homepage Loads", "browser")
     .open("https://flagpolejs.github.io/flagpole")
     .next(async (context) => {
+      const getStarted = await context.waitForHavingText("a", "Get Started");
+      await getStarted.click();
+      // wait for the scroll
+      await context.waitForExists("body.sticky");
       const currentUrl = context.currentUrl;
-      context.assert(currentUrl).includes("/setup");
+      context.comment(currentUrl);
+      context.assert(currentUrl).includes("what-is-flagpole");
     });
 });
