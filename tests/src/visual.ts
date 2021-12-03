e579174f0ea097e6ae1760535a34c309eba91836
Nicholas Calaway <ncalaway@echelonfit.com>
2021-12-03 08:31:36 -0500

    Trying a file path

--- tests/src/visual.ts
+++ tests/src/visual.ts
@@ -23,6 +23,11 @@ suite
   .next("Screenshot", async (context) => {
     await context.waitForExists("img[alt='Debian']");
     context
-      .assert("Homepage matches control screenshot", await context.screenshot())
+      .assert(
+        "Homepage matches control screenshot",
+        await context.screenshot(
+          "/home/runner/work/flagpole/flagpole/tests/images/homepage.png"
+        )
+      )
       .looksLike("@homepage", "5");
   });
