dff22d51dc4246adfe6c879a7131f0178df2531b
Jason Byrne <jbyrne@echelonfit.com>
2022-02-06 10:43:28 -0500

    export all scenario types

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,17 @@ import { AppiumResponse } from "./appium/appium-response";
 import { AppiumScenario } from "./appium/appium-scenario";
 import { HtmlScenario } from "./html/html-scenario";
 import { JsonScenario } from "./json/json-scenario";
+import { ResourceScenario } from "./resource/resource-scenario";
+import { XmlScenario } from "./xml/xml-scenario";
+import { RssScenario } from "./xml/rss-scenario";
+import { AtomScenario } from "./xml/atom-scenario";
+import { SoapScenario } from "./xml/soap-scenario";
+import { ImageScenario } from "./visual/image-scenario";
+import { BrowserScenario } from "./puppeteer/browser-scenario";
+import { ExtJsScenario } from "./puppeteer/extjs-scenario";
+import { HeadersScenario } from "./headers/headers-scenario";
+import { FfprobeScenario } from "./media/ffprobe-scenario";
+import { MediaStreamValidatorScenario } from "./media/media-stream-validator-scenario";
 
 // Aliases
 const execution = FlagpoleExecution.global;
@@ -66,4 +77,15 @@ export {
   HtmlScenario,
   JsonScenario,
   HlsScenario,
+  ResourceScenario,
+  XmlScenario,
+  AtomScenario,
+  SoapScenario,
+  RssScenario,
+  BrowserScenario,
+  ExtJsScenario,
+  ImageScenario,
+  HeadersScenario,
+  FfprobeScenario,
+  MediaStreamValidatorScenario,
 };
